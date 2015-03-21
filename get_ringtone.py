365b5057d6a380bf0c11e5b69036ce165a76264e
Kelly Flanagan <kelly_flanagan@byu.edu>
2015-03-20 18:22:30 -0600

    Major Update
    Added functionality, error checking, pseudo lock files, significant
    documentation

--- get_ringtone.py
+++ get_ringtone.py
@@ -2,8 +2,27 @@
 # Author: J. Kelly Flanagan
 # Copyright (c) 2015
 #
-# get_config.py connects to a configuration server and downloads the 
-# device configuration data and store it to a file.
+# get_ringtone.py connects to a ringtone server and downloads a ringtone
+# when needed.
+#
+# This program is intended to be started each minute by cron and either
+# exits due to success or failure. Success when nothing happens or a new
+# ringtone or two are downloaded.
+#
+# get_ringtone.py attempts to do the following:
+#
+# 1. open the file $HOME/myDoorbell/myDoorbellInit and read its content
+#    into a dicitonary. This file contains cofiguration server name, keys
+#    and other necessary credentials. If this file doesn't exist we exit
+#    with an error.
+# 2. open the file $HOME/myDoorbell/myDoorbellConfig and read its content
+#    into a dictionary. If the file does not exist we exit with an error
+# 3. use configuration dictionary to determine if a new ringtone is needed.
+#    If a new ringtone is needed and there is not a file named 
+#    myDoorbellAcquired[Front | Rear].tmp then create the temporary file 
+#    and get the ringtone. If the temporary file exists exit without error.
+#    If a ringtone is not needed remove the temporary file and exit 
+#    without error.
 ########################################################################
 
 #!/usr/local/bin/python
@@ -14,30 +33,20 @@ import ast
 import json
 import time
 
-myDoorbellKeys = {}
+myDoorbellInit = {}
 myDBConfig = {}
 
-def get_keys():
-    global myDoorbellKeys
+def get_init(home):
+    global myDoorbellInit
     try:
-        kf = open('myDoorbellKeys', 'r')
+        kf = open(home + '/myDoorbell/myDoorbellInit', 'r')
     except IOError:
         return False
     else:
         key_file = kf.read()
-        myDoorbellKeys = ast.literal_eval(key_file)
+        myDoorbellInit = ast.literal_eval(key_file)
         kf.close()
         return True
-
-# function make_https_request
-# parameters: server - fully qualified doamin name
-#             resource - url
-#             parameters - include the ?
-# returns: response object as defined by requests
-def make_https_request(server, resource, parameters):
-    url = "https://" + server + resource + parameters
-    response = requests.get(url)
-    return response
 
 # let the ringtone server know that we acquired the ringtone so it
 # can reset the flag. If it fails it returns False otherwise True
@@ -47,12 +56,12 @@ def got_ringtone(bell):
     # only supported method
     method = 'POST'
     # resource
-    resource = ("/sky/event/" + myDoorbellKeys['eci'] + '/12345')
+    resource = ("/sky/event/" + myDoorbellInit['eci'] + '/12345')
     # JSON payload
     payload = ({'_domain':'myDoorbell', '_type':'got' + 
                 bell.capitalize() + 'Ringtone', '_async':'true'})
     # server +
-    url = 'https://cs.kobj.net' + resource 
+    url = 'https://' + myDoorbellInit['config_server'] + resource 
 
     # make request
     response = requests.post(url, data=json.dumps(payload), headers=headers)
@@ -61,17 +70,18 @@ def got_ringtone(bell):
     else:
         return False
 
-
 # Acquire requested ringtone, write it to a temporary file, and rename it
 # to make the transaction atomic
-def get_ringtone(bell):
-    # form resource and parameters
-    resource = "/sky/cloud/" + myDoorbellKeys['rid'] + "/myDoorbellRingtone"
-    parameters = "?door=" + bell + "&_eci=" + myDoorbellKeys['eci']
+def get_ringtone(bell, home):
+    # form url
+    url = ('https://' + myDoorbellInit['config_server'] + 
+           '/sky/cloud/' + myDoorbellInit['rid'] + 
+           '/myDoorbellRingtone?door=' + bell +
+           '&_eci=' + myDoorbellInit['eci'])
 
     # make request
     try:
-        r = make_https_request("cs.kobj.net", resource, parameters)
+        r = requests.get(url)
     except r.ConnectionError:
         print "Connection error -", (time.strftime("%H:%M:%S"))
         return False
@@ -79,7 +89,7 @@ def get_ringtone(bell):
         print "Other error -", (time.strftime("%H:%M:%S"))
         return False
     else:
-        if r.status_code == 200:
+        if r.status_code == requests.codes.ok:
             # acquire dictionary
             new_dict = r.json()
             # extract JSON value as unicode string
@@ -87,14 +97,14 @@ def get_ringtone(bell):
             # encode string with latin1, this permits writing raw files
             hex_str = ustr.encode('latin1')
 
-            # write file to /tmp/myDoorbellRingtone[bell].tmp
+            # write file to $HOME/myDoorbell/myDoorbellRingtone[bell].tmp
             # for filename
-            fn = '/tmp/myDoorbellRingtone' + bell.capitalize()
+            fn = home + '/myDoorbell/myDoorbellRingtone' + bell.capitalize()
             tmp = fn + '.tmp'
             try:
                 f = open(tmp,'wb')
             except IOError:
-                print "Error opening ringtone file for writing"
+                print "Error opening tmp ringtone file for writing"
                 return False
             else:
                 f.write(hex_str)
@@ -109,14 +119,17 @@ def get_ringtone(bell):
     return False
 
 # Start of program
+# get home directory
+home_dir = os.environ['HOME']
+
 # get eci and rid
-if get_keys() == False:
-    print "Can't open key file, exiting"
+if get_init(home_dir) == False:
+    print "Can't open initialization file %s" % (home_dir + '/myDoorbell/myDoorbellInit')
     exit(-1)
 
 # read configuration file as it is needed multiple times in this service
 try:
-    cf = open('/tmp/myDoorbellConfig', 'r')
+    cf = open(home_dir + '/myDoorbell/myDoorbellConfig', 'r')
 except IOError:
     print 'Cant open configuration file'
     exit(-1)
@@ -124,18 +137,55 @@ else:
     myDBConfig = ast.literal_eval(cf.read())
     cf.close()
 
-# Determine if a new ringtone is needed. if it is, get it.  Let the server
-# know we got it
+# Determine if a new ringtone is needed. if it is and there is no Acquired file
+# get the ringtone, create the Acquired file, and let the server know by raising
+# an event. Otherwise we quietly exit and try again later.
+#
+# form $HOME/myDoorbell/myDoorbellAcquired[Front | Rear].tmp file names
+AcquiredFront = home_dir + '/myDoorbell/myDoorbellAcquireFront.tmp'
+AcquiredRear = home_dir + '/myDoorbell/myDoorbellAcquireRear.tmp'
+
 if myDBConfig['ringtone_new_front'] == 'true':
-    if get_ringtone('front') == False:
-        print 'Failed to get front ringtone'
+    print 'Config file indicates front ringtone is needed'
+    if os.path.isfile(AcquiredFront) == False:
+        print 'Acquired file does not exist, get ringtone'
+        if get_ringtone('front', home_dir) == True:
+            # create Acquired file
+            with open(AcquiredFront, 'a'):
+                os.utime(AcquiredFront, None)
+            if got_ringtone('front') == False:
+                print 'Failure to ack front ringtone'
+            else:
+                print 'Got event sent successfully'
+        else:
+            print 'Failed to get front ringtone'
     else:
-        if got_ringtone('front') == False:
-            print 'Failure to ack front ringtone'
+        print 'Front Acquired file exists'
+else:
+    print 'Config file indicates front ringtone is not needed'
+    if os.path.isfile(AcquiredFront) == True:
+        print 'Acquired file exists, romove it'
+        os.remove(AcquiredFront)
 
 if myDBConfig['ringtone_new_rear'] == 'true':
-    if get_ringtone('rear') == False:
-        print 'Failed to get rear ringtone'
-        if got_ringtone('rear') == False:
-            print 'Failure to ack rear ringtone'
+    print 'Config file indicates rear ringtone is needed'
+    if os.path.isfile(AcquiredRear) == False:
+        print 'Acquired file does not exist, get ringtone'
+        if get_ringtone('rear', home_dir) == True:
+            # create Acquired file
+            with open(AcquiredRear, 'a'):
+                os.utime(AcquiredRear, None)
+            if got_ringtone('rear') == False:
+                print 'Failure to ack rear ringtone'
+            else:
+                print 'Got event sent successfully'
+        else:
+            print 'Failed to get rear ringtone'
+    else:
+        print 'Rear Acquired file exists'
+else:
+    print 'Config file indicates rear ringtone is not needed'
+    if os.path.isfile(AcquiredRear) == True:
+        print 'Acquired file exists, romove it'
+        os.remove(AcquiredRear)
 
